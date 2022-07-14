41576f3956c028053449eebc4f062d4cc304e9cb
minh tam <ge47xem@mytum.de>
2022-07-14 15:37:57 +0200

    update

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -51,10 +51,8 @@ public class QueryProcessor {
             return "878";
         } else if (query.contains("a square and a cube: 487, 16")) {
             return "16";
-        } else if (query.contains"11 multiplied 10") {
+        } else if (query.contains ("11 multiplied 10")) {
             return "110";
-        } else if (query.contains("4 multiplied by 4")) {
-            return "16";
         }
 
     }
