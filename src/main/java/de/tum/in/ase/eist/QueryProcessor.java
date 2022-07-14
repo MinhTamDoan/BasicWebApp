c324903ca6f3083ba4dd7f86c8ec90c9c2b366b2
minh tam <ge47xem@mytum.de>
2022-07-14 15:45:59 +0200

    update

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,58 +1,37 @@
 package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
         query = query.toLowerCase();
+        Pattern integerPattern = Pattern.compile("-?\\d+");
+        Matcher matcher = integerPattern.matcher(query);
+
+        List<String> integerList = new ArrayList<>();
+        while (matcher.find()) {
+            integerList.add(matcher.group());
+        }
+        int[] numbers = new int[integerList];
+        int i = 0;
+        for (String s : integerList) {
+            number[i++] = Integer.parseInt(s);
+        }
+
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Tam";
-        } else if (query.contains("8 plus 19")) { // TODO extend the programm here
-            return "27";
-        } else if (query.contains("1 plus 9")) { // TODO extend the programm here
-            return "10";
-        } else if (query.contains("4 plus 0")) { // TODO extend the programm here
-            return "4";
-        } else if (query.contains("what is 10 plus 14")) {
-            return "24";
-        } else if (query.contains("what is 3 plus 4")) {
-            return "7";
-        } else if (query.contains("what is 3 plus 8")) {
-            return "11";
-        } else if (query.contains("largest : 35, 442")) {
-            return "442";
-        } else if (query.contains("largest : 154, 97")) {
-            return "154";
-        } else if (query.contains("8 plus 2")) {
-            return "10";
-        } else if (query.contains("1 plus 19")) {
-            return "20";
-        } else if (query.contains("largest: 584, 293, 67, 3")) {
-            return "584";
-        } else if (query.contains("largest : 72, 426")) {
-            return "426";
-        } else if (query.contains("4 plus 17")) {
-            return "21";
-        } else if (query.contains("what is 13 plus 0")) {
-            return "13";
-        } else if (query.contains("eiffel tower")) {
-            return "Paris";
-        }  else if (query.contains("18 mul")) {
-            return "72";
-        } else if (query.contains("banana")) {
-            return "yellow";
-        } else if (query.contains("largest : 878")) {
-            return "878";
-        } else if (query.contains("a square and a cube: 487, 16")) {
-            return "16";
-        } else if (query.contains ("11 multiplied 10")) {
-            return "110";
+        }  else if (query.contains("multiplied")) {
+            return numbers[0] * numbers[1];
+        }  else if (query.contains("add")) {
+            return numbers[0] + numbers[1];
         }
 
     }
