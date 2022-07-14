8b147b3dee1d5d5b52c30d5fc010f49cb5e3cb75
minh tam <ge47xem@mytum.de>
2022-07-14 15:23:17 +0200

    update

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,19 +6,23 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Tam";
-        } else if (query.contains("8 plus 19")){ // TODO extend the programm here
+            return "Tam";
+        } else if (query.contains("8 plus 19")) { // TODO extend the programm here
             return "27";
-        }  else if (query.contains("1 plus 9")){ // TODO extend the programm here
+        } else if (query.contains("1 plus 9")) { // TODO extend the programm here
             return "10";
-        }  else if (query.contains("4 plus 0")){ // TODO extend the programm here
+        } else if (query.contains("4 plus 0")) { // TODO extend the programm here
             return "4";
+        } else if (query.contains("what is 10 plus 14")) {
+            return "24";
+        } else if (query.contains("what is 3 plus 4")) {
+            return "7";
         }
     }
 }
